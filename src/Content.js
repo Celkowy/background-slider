796e5f6c892ebee147d778c3cc70561ec97255a3
tucelek@gmail.com <tucelek@gmail.com>
2022-05-09 16:13:26 +0200

    fix

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,18 @@
 function Content(props) {
   return (
     <div className="content">
-      <img className="content-background" src={process.env.PUBLIC_URL + `/img/pic${props.slideNumber}.jpg`} />
-      <img className="arrow arrow-right" src="/img/arrow-right.png" onClick={props.nextSlide}></img>
-      <img className="arrow arrow-left" src="/img/arrow-left.png" onClick={props.previousSlide}></img>
+      <img
+        className="content-background"
+        src={process.env.PUBLIC_URL + `/img/pic${props.slideNumber}.jpg`}
+        alt="content-background"
+      />
+      <img className="arrow arrow-right" src="/img/arrow-right.png" onClick={props.nextSlide} alt={arrow - right}></img>
+      <img
+        className="arrow arrow-left"
+        src="/img/arrow-left.png"
+        onClick={props.previousSlide}
+        alt={arrow - left}
+      ></img>
     </div>
   )
 }
